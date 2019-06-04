05ea1f5fd08faedf46059c4a2fb8a2c1a4e63c6d
mbower386 <mbower386@gmail.com>
2019-06-04 00:33:27 -0700

    started working, this is optional

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[, ] mTable = new int[10, 10];
+
+            for (int x = 0; x < mTable.GetLength (0); x++)
+            {
+                for (int y = 0; y < mTable.GetLength (1); y++)
+                {
+                    mTable[x, y] = (x + 1) * (y + 1);
+                }
+            }
+
+             for (int x = 0; x < mTable.GetLength (0); x++)
+            {
+                for (int y = 0; y < mTable.GetLength (1); y++)
+                {
+                    mTable[x, y] = (x + 1) * (y + 1);
+                }
+            }
+        }
+
+    }
+}
